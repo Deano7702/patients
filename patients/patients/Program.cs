1e6bf71d41f7226189950877949c366cfe42e08a
Deano7702 <109179537+Deano7702@users.noreply.github.com>
2022-07-13 08:38:48 +1200

    Add files via upload

---
+++ patients/patients/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace patients
+{
+    /// <summary>
+    /// Date: 13/07/2022
+    /// Creator: Dean Mason
+    /// Desc: Program reads data objects from a JSON file and creates patient objects to store relevant information, which is displayed to the console
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<patient> Patients = new List<patient>(); //List to store patient objects / records
+            int JSONcounter = 0; //Counter to increment through JSON data objects
+
+            try
+            {
+                dynamic jsonFile = JsonConvert.DeserializeObject(File.ReadAllText(@"contactDetails.json")); // Read data from JSON file
+
+                foreach (var contact in jsonFile) //For each JSON data object
+                {
+                    //Store data in respective variables
+                    string full_name = (string)jsonFile[JSONcounter]["full_name"];
+                    string mobile = (string)jsonFile[JSONcounter]["mobile"];
+                    string street = (string)jsonFile[JSONcounter]["address"]["street"];
+                    string city = (string)jsonFile[JSONcounter]["address"]["city"];
+
+                    patient p = new patient(full_name, mobile, street, city); //Create a new patient object with data
+
+                    Patients.Add(p); //Add patient to the patients list
+
+                    JSONcounter++;
+                }
+
+                foreach (var patient in Patients) //For each patient in the patient list
+                {
+                    Console.WriteLine("Full Name: " + patient.FullName + "\nMobile: " + patient.Mobile + "\nStreet: " + patient.Street + "\nCity: " + patient.City + "\n"); //Display respective information to console
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occured retrieving data or the file was not found."); //Prompt error message to console
+            }
+        }
+    } 
+}
---
+++ patients/patients/patient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace patients
+{
+    /// <summary>
+    /// Patient class is a blueprint for create person objects and to record their relevant information
+    /// </summary>
+    class patient
+    {
+        //Class fields
+        private string _fullName;
+        private string _mobile;
+        private string _addressStreet;
+        private string _addressCity;
+
+        /// <summary>
+        /// Constructor, initializes class fields
+        /// </summary>
+        /// <param name="fullName">Stores patients full name</param>
+        /// <param name="mobile">Stores patients mobile number</param>
+        /// <param name="addressStreet">Stores patients street address</param>
+        /// <param name="addressCity">Stores patients city address</param>
+        public patient(string fullName, string mobile, string addressStreet, string addressCity)
+        {
+            _fullName = fullName;
+            _mobile = mobile;
+            _addressStreet = addressStreet;
+            _addressCity = addressCity;
+        }
+
+        /// <summary>
+        /// Getter and setter methods for clients full name
+        /// </summary>
+        public string FullName { get => _fullName; set => _fullName = value; }
+        /// <summary>
+        /// Getter and setter methods for clients mobile
+        /// </summary>
+        public string Mobile { get => _mobile; set => _mobile = value; }
+        /// <summary>
+        /// Getter and setter methods for clients street address
+        /// </summary>
+        public string Street { get => _addressStreet; set => _addressStreet = value; }
+        /// <summary>
+        /// Getter and setter methods for clients city address
+        /// </summary>
+        public string City { get => _addressCity; set => _addressCity = value; }
+    }
+}
